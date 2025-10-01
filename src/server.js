73a1a8a738ff31ded1103291026713ecb70bfbba
Vacancy <tranvantoan9301@gmail.com>
2025-10-01 21:53:12 +0700

    add package mysql2 and test connect database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import mysql from 'mysql2';
 import configViewEngine from './config/viewEngine.js';
 import webRoutes from './routes/web.js';
 
@@ -16,6 +17,24 @@ configViewEngine(app);
 
 // Declare a route handler for the default home page
 app.use('/', webRoutes);
+
+// Create a connection to the database
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+//test the database connection
+db.query('SELECT * FROM Users', (err, results) => {
+    if (err) {
+        console.error('Error executing query:', err);
+        return;
+    }
+    console.log('Query results:', results);
+});
 
 // Start the server on port 3000
 app.listen(port,hostname, () => {
